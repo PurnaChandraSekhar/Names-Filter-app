8eaea068b2ff88b0bc426dfb71cd7c5eaf3b6735
Chandu <chanduchan225@gmail.com>
2019-12-20 21:49:21 +0530

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hey!")
+//SEARCH INPUT
+let filterInput = document.getElementById('searchInput');
+
+filterInput.addEventListener('keyup', (e) => {
+    let filterNames = e.target.value.toUpperCase();
+
+    let names = document.getElementById('names');
+
+    let li = names.querySelectorAll('li.item');
+
+    for(let i=0; i<li.length; i++) {
+       let a = li[i].getElementsByTagName('a')[0];
+
+       if(a.innerHTML.toUpperCase().indexOf(filterNames)> -1){
+           li[i].style.display = "";
+       }else {
+            li[i].style.display = "none";
+       }
+    }
+});
